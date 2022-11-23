a79a461ec9b3442beebc4eb0df5e533939e05ad9
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-11-23 21:49:11 +0300

    Create access.py

---
+++ access.py
@@ -0,0 +1,30 @@
+# Accepts 1 or 2 (1-access, 2-refresh) and returns the desired
+def access(number: int):
+    count = 0
+    with open(r"access.txt", "r") as file:
+        for line in file:
+            count += 1
+            if count == number:
+                return line
+            
+ 
+
+def auth():
+    global token, headers
+    account = requests.get(get_account, headers=headers) # Checks the need to change tokens (amoCRM has no methods for verification)
+    
+    if account.status_code == 401: 
+        auth = requests.post(access_token, data=data).json()
+    
+    if 'id' not in account.json():
+        a_token, r_token = auth['access_token'], auth['refresh_token']
+        
+        with open(r"access.txt", "w") as file: # Overwrite the file 
+            file.write(a_token + '\n' + r_token)
+        with open('tmp/restart.txt', 'tw', encoding='utf-8') as f: # Restart
+            pass
+
+        token = access(1) # .replace('\n', '') Must have
+        headers = {'Authorization': 'Bearer {}'.format(token), 
+           'Content-Type': 'application/json'
+           }
